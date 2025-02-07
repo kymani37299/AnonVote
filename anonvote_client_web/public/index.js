bc9e45fa0b052749901c789531f99e24f2324ae9
Marko Sreckovic <markosrecko97@hotmail.rs>
2025-02-07 20:14:23 +0100

    grpc setup for frontend done

--- anonvote_client_web/public/index.js
+++ anonvote_client_web/public/index.js
@@ -1,1 +1,18 @@
-console.log("Loaded");
+document.getElementById('validateButton').addEventListener('click', function() {
+    const id = document.getElementById('idInput').value || '';
+    fetch('/validate_id', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id: id })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const registrationKey = data.registrationKey;
+        document.getElementById('registrationCode').textContent = registrationKey;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  });
--- anonvote_client_web/src/app.js
+++ anonvote_client_web/src/app.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const anonvote_server_address = 'localhost:50051';
 const app = express();
 const port = 3000;
 
+// gRPC client
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, 'anonvote.proto'));
+const proto = grpc.loadPackageDefinition(packageDefinition).anonvote;
+const client = new proto.AnonVote(anonvote_server_address, grpc.credentials.createInsecure());
+
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
+
+app.post('/validate_id', (req, res) => {
+    const id = req.body.id || '';
+    client.ValidateID({ id }, (error, response) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
+      res.json(response);
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
